d448ea734cc9fdde2d293d7d39573399488e7632
OmPrakash-52 <omp30583@gmail.com>
2021-11-13 12:53:15 +0530

    change4

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, 'dist','app_name')));
+app.use(express.static(path.join(__dirname, 'dist','threuko')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/threuko/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
